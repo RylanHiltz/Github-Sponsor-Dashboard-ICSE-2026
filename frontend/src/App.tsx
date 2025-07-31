15fd9116ddedc2e3139efcff167a7bc11239495d
Rylan Hiltz <rylanhiltz2@gmail.com>
2025-07-30 20:30:33 -0400

    feat: implement routing with Dashboard, Statistics, Leaderboard, and User pages; add apiUrl export; enhance scrollbar styles

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,71 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Dashboard from './pages/Dashboard'
+import Statistics from './pages/statistics/Statistics';
+import Leaderboard from './pages/leaderboard/Leaderboard';
+import User from './pages/users/User';
+import { ConfigProvider, theme as antdTheme } from 'antd'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+export default function App() {
+
+  const [darkMode, setDarkMode] = useState(true); // Or get from local storage
+
+  const theme = {
+    algorithm: darkMode ? antdTheme.darkAlgorithm : antdTheme.defaultAlgorithm,
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ConfigProvider
+      theme={theme}
+    //   token: {
+    //   },
+    //   components: {
+    //     Button: {
+    //       colorPrimary: "#111",
+    //       algorithm: true,
+    //     },
+    //     Input: {
+    //       activeBorderColor: "#111",
+    //       hoverBorderColor: "#111",
+    //       activeShadow: "0 0 0 2px rgba(0,0,0,0.1)",
+    //       algorithm: true,
+    //     },
+    //     Select: {
+    //       activeBorderColor: "#111",
+    //       activeOutlineColor: "rgba(0,0,0,0.1)",
+    //       hoverBorderColor: "#111",
+    //       optionSelectedBg: "#ebebeb",
+    //       algorithm: true,
+    //     },
+    //     Upload: {
+    //       colorPrimary: "#111",
+    //       borderRadius: 10,
+    //       lineWidth: 1.2,
+    //       algorithm: true,
+    //     },
+    //     Checkbox: {
+    //       colorPrimary: "#111",
+    //       colorPrimaryHover: "#111",
+    //     }
+    //   },
+    // }}
+    >
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Dashboard />}>
+            <Route path='' element={<Leaderboard />}></Route>
+            <Route
+              path="/user/:id"
+              element={<User />
+              }
+            />
+            <Route path='statistics' element={<Statistics />}></Route>
+            <Route path='request-user' element={<Statistics />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ConfigProvider >
   )
 }
 
-export default App
