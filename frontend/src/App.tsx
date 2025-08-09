6dc2ca0c04f15a3b11af54430028e87e306a9622
Rylan Hiltz <rylanhiltz2@gmail.com>
2025-08-09 15:19:58 -0400

    fix: correct route for RequestUser component and enhance theme token definitions

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router'
 import Dashboard from './pages/Dashboard'
 import Statistics from './pages/statistics/Statistics';
 import Leaderboard from './pages/leaderboard/Leaderboard';
+import RequestUser from './pages/users/RequestUser.tsx';
 import User from './pages/users/User';
 import { ConfigProvider, theme as antdTheme } from 'antd'
 
 import { ThemeProvider, useTheme } from './context/ThemeContext';
 import { theme as appTheme } from './theme.ts';
+
+import type { AliasToken } from 'antd/es/theme/internal';
+interface CustomToken extends AliasToken {
+  carouselBg: string;
+}
 
 // A new component to access the context provided by ThemeProvider
 const ThemedApp = () => {
@@ -23,9 +29,15 @@ const ThemedApp = () => {
           colorPrimary: theme === 'dark'
             ? themeColors.primary.dark
             : themeColors.primary.light,
-
           colorBgContainer: theme === 'dark' ? '#141414' : '#fff',
-        },
+          carouselBg: theme === 'dark'
+            ? themeColors.carouselBg.dark
+            : themeColors.carouselBg.light,
+          linkHover: theme === 'dark'
+            ? themeColors.text.linkHover.dark
+            : themeColors.text.linkHover.light,
+        } as Partial<CustomToken>,
+
       }}
     >
       <BrowserRouter>
@@ -34,7 +46,7 @@ const ThemedApp = () => {
             <Route path='' element={<Leaderboard />} />
             <Route path="/user/:id" element={<User />} />
             <Route path='statistics' element={<Statistics />} />
-            <Route path='request-user' element={<Statistics />} />
+            <Route path='request-user' element={<RequestUser />} />
           </Route>
         </Routes>
       </BrowserRouter>
