48cceb13deb50be05db260651f9cdafbb8b6900e
Rylan Hiltz <rylanhiltz2@gmail.com>
2025-08-11 20:24:58 -0400

    feat: implement Statistics component with tabbed user and organization statistics

--- frontend/src/pages/statistics/Statistics.tsx
+++ frontend/src/pages/statistics/Statistics.tsx
@@ -1,6 +1,39 @@
 
+import { Tabs } from "antd";
+
 export default function Statistics() {
+
+    const data = [
+        {
+            label: "Overall Statistics",
+            key: "1",
+            children: <></>,
+        },
+        {
+            label: "User Stats",
+            key: "2",
+            children: <UserStatsPage />,
+        },
+        {
+            label: "Organization Stats",
+            key: "3",
+            children: <></>,
+        },
+    ];
+
+
     return (
-        <div>Statistics</div>
+        <Tabs
+            type="card"
+            items={data}
+        />
+    )
+}
+
+const UserStatsPage = () => {
+    return (
+        <>
+            <div>User Statistics</div>
+        </>
     )
 }
